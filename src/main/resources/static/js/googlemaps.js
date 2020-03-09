17e28afe16acf7fb4656fbfdd5196438735329d8
Michael Male <michaelmale31@gmail.com>
2020-03-09 03:08:38 +0000

    JavaScript now consumes a GET Request

--- src/main/resources/static/js/googlemaps.js
+++ src/main/resources/static/js/googlemaps.js
@@ -15,7 +15,28 @@
  * under the License.
  */
 
-function initMap() {
+/**
+ * An asynchronous function, that uses the JavaScript Fetch API to perform an HTTP GET Request to an API containing
+ * all Points Of Interest. The current utilisation of this is as a helper function to support initMap() in creating
+ * markers.
+ * @param url   The URL of a RESTful API that the request is to be made to.
+ * @returns {Promise<Array>} A promise that should contain an array containing the Points of Interest.
+ */
+async function getPointsOfInterest(url) {
+    const response = await fetch(url);
+
+    return response.json();
+}
+
+/**
+ * Performs initialisation of the Google Map, using the Google Maps for JavaScript API. The function also generates
+ * markers from an array of them.
+ *
+ * @returns {Promise<void>} Promise for the initialisation of the Google Map.
+ */
+async function initMap() {
+    const allPointsOfInterest = await getPointsOfInterest('/poi');
+    console.log(allPointsOfInterest); // Debug data to confirm correct POIs have been entered.
     const dyfiWildlifeCentre = {lat: 52.568774, lng: -3.918031};
     const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
