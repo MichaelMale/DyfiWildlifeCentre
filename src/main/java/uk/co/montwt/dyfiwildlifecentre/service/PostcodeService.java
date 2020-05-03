509bcfa7203ddcfe5970b311bc4673591e9775ae
Michael Male <michaelmale31@gmail.com>
2020-05-03 13:27:10 +0100

    Minor refactory to Postcode Service

--- src/main/java/uk/co/montwt/dyfiwildlifecentre/service/PostcodeService.java
+++ src/main/java/uk/co/montwt/dyfiwildlifecentre/service/PostcodeService.java
@@ -20,6 +20,7 @@ package uk.co.montwt.dyfiwildlifecentre.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.HttpStatus;
 import uk.co.montwt.dyfiwildlifecentre.exception.PostcodeException;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -44,15 +45,39 @@ import java.nio.charset.StandardCharsets;
  */
 public class PostcodeService {
 
+    /*
+    Endpoint to the postcodes.io API, a geo-coding API for validation and
+    conversion of UK postcodes to coordinates, and vice versa.
+
+    License: MIT License Copyright (c) 2017 Ideal Postcodes
+    Source: https://github.com/ideal-postcodes/postcodes.io
+     */
+    private static final String POSTCODES_API_LINK = "https://api.postcodes" +
+            ".io/postcodes/";
+
+    /**
+     * Helper method that encodes a postcode to the UTF-8 standard, and
+     * replaces whitespace with an ASCII representation of it.
+     *
+     * @param postcode  String containing the postcode to be encoded.
+     * @return  String containing encoded output
+     */
+    private static String encodePostcode(String postcode) {
+        return URLEncoder.encode(postcode, StandardCharsets.UTF_8).replace(
+                "+", "%20");
+    }
+
     /**
      * A helper method, that queries the API with a suffix and returns its
      * result.
+     *
      * @param suffix    Suffix to query the API.
      * @return  Object of type JsonNode containing the result of such query.
      * @throws IOException  If there is an error when querying the API.
+     * @see com.fasterxml.jackson
      */
     private static JsonNode getAPIResult(String suffix) throws IOException {
-        URL obj = new URL("https://api.postcodes.io/postcodes/" + suffix);
+        URL obj = new URL(POSTCODES_API_LINK + suffix);
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         BufferedReader in =
@@ -71,31 +96,30 @@ public class PostcodeService {
 
     /**
      * Checks whether the postcode is a valid postcode.
+     *
      * @param postcode  Postcode to be validated.
      * @return  True if the postcode is valid, false if not.
      * @throws IOException  If there is an error when querying the API.
      */
     static boolean validatePostcode(String postcode) throws IOException {
-        final String encodedPostcode = URLEncoder.encode(postcode,
-                StandardCharsets.UTF_8).replace("+", "%20");
-
-        return getAPIResult(encodedPostcode + "/validate").asBoolean();
+        return getAPIResult(encodePostcode(postcode) + "/validate")
+                .asBoolean();
     }
 
     /**
      * Geocodes a given postcode.
+     *
      * @param postcode  String containing the postcode.
      * @return Point2D with a double containing the latitude and the
      *         longitude of the postcode.
      * @throws IOException  If there is an error when querying the API.
      * @throws PostcodeException    If the postcode is invalid.
      */
-    static Point2D.Double getCoordinatesFromPostcode(@NotNull String postcode) throws IOException, PostcodeException {
+    static Point2D.Double getCoordinatesFromPostcode(@NotNull String postcode)
+            throws IOException, PostcodeException {
         if (!validatePostcode(postcode)) throw new PostcodeException(postcode
                 , "Invalid postcode");
-        final String encodedPostcode = URLEncoder.encode(postcode,
-                StandardCharsets.UTF_8).replace("+", "%20");
-        JsonNode result = getAPIResult(encodedPostcode);
+        JsonNode result = getAPIResult(encodePostcode(postcode));
         return new Point2D.Double(result.get("latitude").asDouble(),
                 result.get("longitude").asDouble());
     }
